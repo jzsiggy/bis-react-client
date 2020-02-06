8eddf859a6d8937327e9b4ef6160da25d7c063f4
Joao Zsigmond <jzsiggy@gmail.com>
2020-02-06 18:46:09 -0200

    contextProvider

--- src/App.js
+++ src/App.js
@@ -3,6 +3,9 @@ import './App.css';
 
 import { Route , Switch } from 'react-router-dom';
 
+
+import ContextProvider from './context/ContextProvider';
+
 import NavBar from './components/navigation/NavBar';
 import LoginForm from './components/authentication/user-login/LoginForm'
 import SignupForm from './components/authentication/user-signup/SignupForm'
@@ -11,12 +14,14 @@ import SignupForm from './components/authentication/user-signup/SignupForm'
 function App() {
   return (
     <div className="App">
-      <NavBar />
-      <Switch>
-        <Route exact path="/" component='' />
-        <Route path="/login" component={LoginForm} />
-        <Route path="/signup" component={SignupForm} />
-      </Switch>
+      <ContextProvider>
+        <NavBar />
+        <Switch>
+          <Route exact path="/" component='' />
+          <Route path="/login" component={LoginForm} />
+          <Route path="/signup" component={SignupForm} />
+        </Switch>
+      </ContextProvider>
     </div>
   );
 }
