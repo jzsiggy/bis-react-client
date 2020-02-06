92d65951c2e70b25eaeb98761a5e629cb556e2b5
Joao Zsigmond <jzsiggy@gmail.com>
2020-02-05 22:28:09 -0200

    App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+import { Route , Switch } from 'react-router-dom';
+
+import NavBar from './components/navigation/NavBar';
+import LoginForm from './components/authentication/user-login/LoginForm'
+import SignupForm from './components/authentication/user-signup/SignupForm'
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Switch>
+        <Route exact path="/" component='' />
+        <Route path="/login" component={LoginForm} />
+        <Route path="/signup" component={SignupForm} />
+      </Switch>
     </div>
   );
 }
