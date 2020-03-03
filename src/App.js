dc8c67353263109c9739e5a6a492a5f3153007b1
Joao Zsigmond <jzsiggy@gmail.com>
2020-03-03 18:05:58 -0300

    Adding components to app.js

--- src/App.js
+++ src/App.js
@@ -6,20 +6,29 @@ import { Route , Switch } from 'react-router-dom';
 
 import ContextProvider from './context/ContextProvider';
 
+import Cart from './components/cart/Cart';
+
 import NavBar from './components/navigation/NavBar';
 import LoginForm from './components/authentication/user-login/LoginForm'
 import SignupForm from './components/authentication/user-signup/SignupForm'
+import ProductList from './components/products/menu/ProductList';
+import ProductDetail from './components/products/product-detail/ProductDetail';
+import Checkout from './components/checkout/Checkout';
 
 
 function App() {
   return (
     <div className="App">
       <ContextProvider>
+        <ProductDetail/>
         <NavBar />
         <Switch>
           <Route exact path="/" component='' />
           <Route path="/login" component={LoginForm} />
           <Route path="/signup" component={SignupForm} />
+          <Route path="/menu" component={ProductList} />
+          <Route path="/user/cart/:id" component={Cart} />
+          <Route path="/checkout" component={Checkout} />
         </Switch>
       </ContextProvider>
     </div>
