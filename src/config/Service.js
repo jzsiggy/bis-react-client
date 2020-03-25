6e38a47407b2cccaef1a2200f89e25e880834529
Joao Zsigmond <jzsiggy@gmail.com>
2020-03-25 20:44:18 -0300

    Adding heroku backend api url

--- src/config/Service.js
+++ src/config/Service.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const Service = axios.create({
   baseURL : `https://bis-api.herokuapp.com/user`,
+  // baseURL : `http://localhost:5000/user`,
   validateStatus : function (status) {
     return status >= 200 && status < 300;
   },
