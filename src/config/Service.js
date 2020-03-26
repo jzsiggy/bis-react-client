e1a380ac1346b06230eec51002b4f4db1fdb776c
Joao Zsigmond <jzsiggy@gmail.com>
2020-03-26 12:17:31 -0300

    Changing api req to http

--- src/config/Service.js
+++ src/config/Service.js
@@ -1,13 +1,12 @@
 import axios from 'axios';
 
 const Service = axios.create({
-  baseURL : `https://bis-api.herokuapp.com/user`,
+  baseURL : `http://bis-api.herokuapp.com/user`,
   // baseURL : `http://localhost:5000/user`,
   validateStatus : function (status) {
     return status >= 200 && status < 300;
   },
   withCredentials : true,
-  // headers: {'Connection': 'keep-alive'}
 });
 
 export default Service;
